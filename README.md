8f0adaedb3da72604888bd6ce4c91411863aa94d
Audifaz <emilio.aguila@hotmail.com>
2021-07-19 22:17:03 -0500

    README udpated and now the code inside interpreter.py ix executed inside the main function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This script works with the help of jinja2 and docx2txt libraries. Therefore they are needed.
 
+At the moment, it needs to be changed directly in the code, to which PATH you want to access. 
+
+Also, the code is executed sequentually, without any function call except main().
--- interpreter.py
+++ interpreter.py
@@ -1,23 +1,27 @@
-#TODO Create File separation
-#TODO Add the option to dinamically select which word document will be used
 #TODO Create class and function
+#TODO Add the option to dinamically select which word document will be used
+
 import docx2txt
 import jinja2
 
-PATH='template.html'
+def main():
+    PATH='template.html'
 
-text=docx2txt.process("./WordDocs/Pensamientos 7.docx")
+    text=docx2txt.process("./WordDocs/Pensamientos 7.docx")
 
-templateLoader=jinja2.FileSystemLoader(searchpath="./Templates")
+    templateLoader=jinja2.FileSystemLoader(searchpath="./Templates")
 
-templateEnv=jinja2.Environment(loader=templateLoader)
+    templateEnv=jinja2.Environment(loader=templateLoader)
 
-template=templateEnv.get_template(PATH)
+    template=templateEnv.get_template(PATH)
 
-output=template.render(variable=text)
+    output=template.render(variable=text)
 
-encoded_unicode=output.encode("utf8")
+    encoded_unicode=output.encode("utf8")
 
-f=open("./Output/pensamiento.html","wb")
-f.write(encoded_unicode)
-f.close()
+    f=open("./Output/pensamiento.html","wb")
+    f.write(encoded_unicode)
+    f.close()
+
+if __name__ == "__main__":
+    main()
