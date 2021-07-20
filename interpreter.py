321b80095d57f9e84dd6b5ad0775eea9eb54243f
Audifaz <emilio.aguila@hotmail.com>
2021-07-19 22:06:22 -0500

    Documents have been divided in specialized directories and the name of the python file has changed

--- interpreter.py
+++ interpreter.py
@@ -1,12 +1,14 @@
+#TODO Create File separation
 #TODO Add the option to dinamically select which word document will be used
+#TODO Create class and function
 import docx2txt
 import jinja2
 
 PATH='template.html'
 
-text=docx2txt.process("Pensamientos 7.docx")
+text=docx2txt.process("./WordDocs/Pensamientos 7.docx")
 
-templateLoader=jinja2.FileSystemLoader(searchpath="./")
+templateLoader=jinja2.FileSystemLoader(searchpath="./Templates")
 
 templateEnv=jinja2.Environment(loader=templateLoader)
 
@@ -16,6 +18,6 @@ output=template.render(variable=text)
 
 encoded_unicode=output.encode("utf8")
 
-f=open("pensamiento.html","wb")
+f=open("./Output/pensamiento.html","wb")
 f.write(encoded_unicode)
 f.close()
