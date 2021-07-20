d337673bbcf8958bdd9305ca1ddf878d042f1acc
Audifaz <emilio.aguila@hotmail.com>
2021-07-19 19:13:32 -0500

    Unicode characters now work correctly

--- reader.py
+++ reader.py
@@ -1,24 +1,21 @@
 import docx2txt
 import jinja2
 
+
+PATH='template.html'
+
 text=docx2txt.process("Pensamientos 7.docx")
-
-print((text))
-
-f=open("test.txt","w")
-#f.write(text)
-f.close()
-
-path='template.html'
 
 templateLoader=jinja2.FileSystemLoader(searchpath="./")
 
 templateEnv=jinja2.Environment(loader=templateLoader)
 
-template=templateEnv.get_template(path)
+template=templateEnv.get_template(PATH)
 
 output=template.render(variable=text)
 
-f=open("pensamiento.html","w")
-f.write(output)
+encoded_unicode=output.encode("utf8")
+
+f=open("pensamiento.html","wb")
+f.write(encoded_unicode)
 f.close()
